3240bd3abf4fb9ed343646be81e1fd8233d06ee1
petermr <peter.murray.rust@googlemail.com>
2024-09-29 22:30:59 +0100

    merged pmr_aug

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ requirements = [
 setup(
     name='amilib',
     url='https://github.com/petermr/amilib',
-    version='0.3.7',
+    version='0.3.8',
     description='document and dictionary download, cleaning, management',
     long_description_content_type='text/markdown',
     long_description=readme,
